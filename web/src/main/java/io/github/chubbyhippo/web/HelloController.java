6564d30a7e194e27de2370e751adde617bd0962d
chubbyhippo <mkmsacc@outlook.com>
2025-06-28 18:41:59 +0700

    Add base `/web` path to HelloController with `@RequestMapping`

--- web/src/main/java/io/github/chubbyhippo/web/HelloController.java
+++ web/src/main/java/io/github/chubbyhippo/web/HelloController.java
@@ -1,9 +1,11 @@
 package io.github.chubbyhippo.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/web")
 public class HelloController {
 
     @GetMapping("/hello")
